778ddf083ae85349c06829b8e5b341be0cc41817
Harshit <harshitpandit3525@gmail.com>
2026-03-31 23:42:31 +0530

    Create users routes using Express Router with basic CRUD endpoints

---
+++ classroom/routes/user.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+
+// ===============================================
+
+// Users
+// Index - users
+router.get("/", (req, res) => {
+    res.send("GET for users");
+});
+
+// Show - users
+router.get("/:id", (req, res) => {
+    res.send("GET for user id");
+});
+
+// CREATE/ADD/POST - users
+router.post("/", (req, res) => {
+    res.send("POST for users");
+});
+
+// DELETE - users
+router.delete("/:id", (req, res) => {
+    res.send("DELETE for user id");
+});
+
+module.exports = router;
