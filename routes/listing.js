1e19a8d99b12d1627dbbe1065a7093ff67b1ab0c
Harshit <harshitpandit3525@gmail.com>
2026-04-13 10:12:23 +0530

    refactor: organize listing routes with router.route chaining

--- routes/listing.js
+++ routes/listing.js
@@ -7,20 +7,38 @@ const listing = require('../models/listing.js');
 const { isLoggedIn,isOwner,validateListing } = require("../middleware.js")
 const listingController = require('../controllers/listing.js');
 
-// Index route(list all the listings)  Read route
-router.get('/', listingController.index);
-// new route
-router.get('/new',isLoggedIn,listingController.renderNewForm )
-// Create route
-router.post('/', isLoggedIn,validateListing, wrapAsync(listingController.createListing));
+//router.route() is used to chain multiple route handlers for a specific path. 
+router.route('/')
+    //index route(list all the listings)  Read route
+    .get(listingController.index)
+    // Create route
+    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
 
-//show route
-router.get('/:id', wrapAsync(listingController.showListing));
-// Edit Route
-router.get('/:id/edit',isLoggedIn,isOwner, wrapAsync(listingController.renderEditForm));
-// Update Route
-router.put('/:id',isLoggedIn, isOwner,validateListing, wrapAsync(listingController.updateListing));
-// Delete Route
-router.delete('/:id',isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
-
-module.exports = router;
+    // new route
+    router.get('/new',isLoggedIn,listingController.renderNewForm )
+    // Edit Route
+    router.get('/:id/edit',isLoggedIn,isOwner, wrapAsync(listingController.renderEditForm));
+    
+    router.route('/:id')
+    //show route
+    .get(wrapAsync(listingController.showListing))
+    //update route
+    .put(isLoggedIn, isOwner,validateListing, wrapAsync(listingController.updateListing))
+    //delete route
+    .delete(isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
+    module.exports = router;
+    
+    
+    // Index route(list all the listings)  Read route
+    // router.get('/', listingController.index);
+    // Create route
+    // router.post('/', isLoggedIn,validateListing, wrapAsync(listingController.createListing));
+    
+    //show route
+    // router.get('/:id', wrapAsync(listingController.showListing));
+    // Update Route
+    // router.put('/:id',isLoggedIn, isOwner,validateListing, wrapAsync(listingController.updateListing));
+    // Delete Route
+    // router.delete('/:id',isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
+    
+    
