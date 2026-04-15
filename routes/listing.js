7bbbbf13420a256c165d1021712d4754ec477b05
Harshit <harshitpandit3525@gmail.com>
2026-04-15 10:31:40 +0530

    feat: add multer-based image upload handling to listing route

--- routes/listing.js
+++ routes/listing.js
@@ -6,13 +6,23 @@ const listing = require('../models/listing.js');
 //middleware
 const { isLoggedIn,isOwner,validateListing } = require("../middleware.js")
 const listingController = require('../controllers/listing.js');
+//multipart form data handling for file uploads using multer
+const multer = require('multer');
+const {storage} = require('../cloudConfig.js');
+const upload = multer({ storage });
+// const upload = multer({ dest: 'uploads/' }); // Temporary storage for uploaded files
 
 //router.route() is used to chain multiple route handlers for a specific path. 
 router.route('/')
     //index route(list all the listings)  Read route
     .get(listingController.index)
     // Create route
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    .post(isLoggedIn, validateListing,
+        upload.single('listing[image][url]'),
+         wrapAsync(listingController.createListing));
+    // .post((req, res) => {
+    //     res.send(req.file);}); // This will return the uploaded file information
+    
 
     // new route
     router.get('/new',isLoggedIn,listingController.renderNewForm )
